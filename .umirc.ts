2bf929d726b48c1e398e998f41424b72b1cfaef2
yuqi.pyq <yuqi.pyq@antfin.com>
2019-09-27 18:44:05 +0800

    docs: add home page

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,7 @@ import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
 const config: IConfig =  {
+  outputPath: './docs',
   treeShaking: true,
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
